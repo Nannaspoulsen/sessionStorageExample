013170725bc787a291ef35b927d3be91cbfdec21
Nannaspoulsen <Nannaspoulsen@hotmail.com>
2025-02-25 17:09:21 +0100

    qerty

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,35 @@
 
 // First - store a value using sessionStorage.setItem("key", "value")
 
-// Now get it and display it somewhere
+// sessionStorage.setItem("favColor", "red");
+
+// // Now get it and display it somewhere
+
+// const currentUserEl = document.querySelector(".currentUser");
+// currentUserEl.textContent = sessionStorage.getItem("favColor");
 
 // Delete the sessionStorage and the code above and do the main task:
 
 // Get the form
+const formEl = document.querySelector("form");
 
 // Listen for submit event, prevent default, store the first+last name in sessionStorage
+formEl.addEventListener("submit", function (e) {
+    //prevent default sørger for at formularenn IKKE refresher sig selv
+    e.preventDefault();
+const userFirstName = formEl.formFirstName.value;
+const userLastName = formEl.formLastName.value;
+
+sessionStorage.setItem("usernname", userFirstName + userLastName);
+})
 
 // Get the .currentUser and store the value of the sessionStorage in there
 
+const currentUserEl = document.querySelector(".currentUser");
+currentUserEl.textContent = sessionStorage.getItem("username");
+
 // Now try getting the sessionStorage from anotherfile.html
+
+
 
 // If you have time: try to write the pseudocode for handling the light/darkmode colorswitch. Tip: I've setup the css so that all you have to do to go to darkmode is change the css-variables lines 2-3, have a look
